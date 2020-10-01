a3b70ef2e4d97e69f3395438019c5352284b923f
23eliu <23eliu@athenian.org>
2020-10-01 00:58:02 +0000

    finished 1st method

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,8 +5,18 @@ public class MyMain {
     // Returns in the answer as a double corresponding to the percentage
     // For example, 75.5% would be 75.5
     public static double probabilityOneSix() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int count = 0;
+        int time = 10000;
+        for (int i = 1; i <= time; i++) {
+            for (int j = 1; j <= 6; j++) {
+                if ((int) ((Math.random() * 6) + 1) == 6) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        double fin = count/100;
+        return fin;
     }
 
     // Calculate the probability of rolling at least two 6's when rolling 
